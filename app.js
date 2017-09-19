4419674b5420e70f5ffc142aab4c1d74c50a3388
Andy Lim <trilliondollarmark@gmail.com>
2017-09-20 06:30:55 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Use native Promises
+mongoose.Promise = global.Promise;
+
+mongoose.connection.openUri(config.database);
+//mongoose.connect(config.database);
+
+
+const app = express();
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database' + config.database);
+});
+
+const users = require('./routes/users');
+
+//port number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endopoint')
+});
+
+//start server
+app.listen(port, () => {
+   console.log('Server started on port ' + port);
+   //console.log('Connected to database mongodb://localhost:27017/meanapp')
+});
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/meanauth',
+    //mongoose.connection.openUri('mongodb://127.0.0.1/bookstore')
+    secret: 'MysEcret'
+}
